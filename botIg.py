8b2f826b4f361c7daaf29fdfa59ca5b85d34df27
Aryan Ritesh <aryanrtsh@gmail.com>
2021-10-19 01:41:34 +0530

    Ready to use, added limiting parameters too

--- botIg.py
+++ botIg.py
@@ -1,11 +1,12 @@
 from instapy import InstaPy
+from instapy import smart_run
 # name=os.environ.get('username')
 # passw=os.environ.get('pass')
-session=InstaPy(username="radhikaa.2103")
+session=InstaPy(headless_browser=True)
 session.login()
-#session.do_comment(enabled=True, percentage=100)
-#session.set_comments(['Beauty','love your content so much','Amazing post'])
-session.set_do_follow(enabled=True,percentage=30,times=1)
+with smart_run(session):
+    session.set_relationship_bounds(enabled=True,delimit_by_numbers=True,max_followers=4000,min_followers=500,min_following=200)
+session.set_do_follow(enabled=True,percentage=70,times=1)
 session.follow_commenters(['jazzzyy.x','radhikasethh','shirleysetia','memezar'],amount=10,daysold=200,max_pic=20,sleep_delay=600,interact=False)
 #session.set_user_interact(amount=5, randomize=True, percentage=100, media='Photo')
 session.like_by_tags(['fashion','reels','music','musician','indianreels','indiansongs','memes'],amount=10)
